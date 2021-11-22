090bf2601de4f2fa0a5a0aec68fb5f9425ee4c0f
Tiaw <delemotte.laurent@gmail.com>
2021-11-22 14:12:41 +0100

    Ajout gestion taille colonne

--- src/main/java/Grille.java
+++ src/main/java/Grille.java
@@ -2,15 +2,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Grille {
+    private int ligneMaximum = 6;
+
     private List<StatutCase> colonne = new ArrayList<>();
 
-    public StatutCase statutCase(int colonne, int ligne)
-    {
+    public StatutCase statutCase(int colonne, int ligne) {
         return ligne <= this.colonne.size() ? this.colonne.get(ligne) : StatutCase.VIDE;
     }
 
-    public void ajouterJeton(int colonne, CouleurJeton couleurJeton) {
+    public boolean ajouterJeton(int colonne, CouleurJeton couleurJeton) {
+        if (this.colonne.size() >= ligneMaximum) {
+            return false;
+        }
+
         StatutCase statut = couleurJeton == CouleurJeton.JAUNE ? StatutCase.JAUNE : StatutCase.ROUGE;
         this.colonne.add(statut);
+
+        return true;
     }
 }
--- src/test/java/GrilleTest.java
+++ src/test/java/GrilleTest.java
@@ -3,41 +3,65 @@ import org.junit.jupiter.api.Test;
 
 public class GrilleTest {
     @Test
-    public void Si_On_Ajoute_Rouge_En_0_Sur_Une_Grille_Vide_Alors_Le_Statut_De_La_Case_0_0_Est_Rouge(){
+    public void Si_On_Ajoute_Rouge_En_0_Sur_Une_Grille_Vide_Alors_Le_Statut_De_La_Case_0_0_Est_Rouge() {
         Grille maGrille = new Grille();
 
         maGrille.ajouterJeton(0, CouleurJeton.ROUGE);
 
-        StatutCase valeurReelle = maGrille.statutCase(0,0);
-        Assertions.assertEquals(StatutCase.ROUGE,valeurReelle);
+        StatutCase valeurReelle = maGrille.statutCase(0, 0);
+        Assertions.assertEquals(StatutCase.ROUGE, valeurReelle);
     }
 
     @Test
-    public void Si_Ajoute_Joue_Jaune_En_0_Sur_Une_Grille_Vide_Alors_Le_Statut_De_La_Case_0_0_Est_Jaune(){
+    public void Si_Ajoute_Joue_Jaune_En_0_Sur_Une_Grille_Vide_Alors_Le_Statut_De_La_Case_0_0_Est_Jaune() {
         Grille maGrille = new Grille();
 
-        maGrille.ajouterJeton(0,CouleurJeton.JAUNE);
+        maGrille.ajouterJeton(0, CouleurJeton.JAUNE);
 
-        StatutCase valeurReelle = maGrille.statutCase(0,0);
-        Assertions.assertEquals(StatutCase.JAUNE,valeurReelle);
+        StatutCase valeurReelle = maGrille.statutCase(0, 0);
+        Assertions.assertEquals(StatutCase.JAUNE, valeurReelle);
     }
 
     @Test
-    public void Si_On_A_Pas_De_Pion_Dans_Une_Case_elle_Est_Vide(){
+    public void Si_On_A_Pas_De_Pion_Dans_Une_Case_elle_Est_Vide() {
         Grille maGrille = new Grille();
 
-        StatutCase valeurReelle = maGrille.statutCase(0,1);
-        Assertions.assertEquals(StatutCase.VIDE,valeurReelle);
+        StatutCase valeurReelle = maGrille.statutCase(0, 1);
+        Assertions.assertEquals(StatutCase.VIDE, valeurReelle);
     }
 
     @Test
-    public void Si_On_Ajoute_En_0_Sur_Un_Pion_Existant_Alors_Le_Statut_De_La_Case_0_1_Est_Rouge(){
+    public void Si_On_Ajoute_En_0_Sur_Un_Pion_Existant_Alors_Le_Statut_De_La_Case_0_1_Est_Rouge() {
         Grille maGrille = new Grille();
 
         maGrille.ajouterJeton(0, CouleurJeton.ROUGE);
-        maGrille.ajouterJeton(0,CouleurJeton.JAUNE);
+        maGrille.ajouterJeton(0, CouleurJeton.JAUNE);
 
-        Assertions.assertEquals(StatutCase.ROUGE, maGrille.statutCase(0,0));
-        Assertions.assertEquals(StatutCase.JAUNE, maGrille.statutCase(0,1));
+        Assertions.assertEquals(StatutCase.ROUGE, maGrille.statutCase(0, 0));
+        Assertions.assertEquals(StatutCase.JAUNE, maGrille.statutCase(0, 1));
+    }
+
+    @Test
+    public void Si_On_A_Pu_Ajouter_Un_Pion_Alors_La_Methode_Retourne_True() {
+        Grille maGrille = new Grille();
+
+        boolean reussite = maGrille.ajouterJeton(0, CouleurJeton.JAUNE);
+
+        Assertions.assertTrue(reussite);
+    }
+
+    @Test
+    public void Si_On_Ajoute_Trop_De_Pion_Alors_La_Methode_Retourne_False() {
+        Grille maGrille = new Grille();
+        maGrille.ajouterJeton(0, CouleurJeton.ROUGE);
+        maGrille.ajouterJeton(0, CouleurJeton.ROUGE);
+        maGrille.ajouterJeton(0, CouleurJeton.ROUGE);
+        maGrille.ajouterJeton(0, CouleurJeton.ROUGE);
+        maGrille.ajouterJeton(0, CouleurJeton.ROUGE);
+        maGrille.ajouterJeton(0, CouleurJeton.ROUGE);
+
+        boolean reussite = maGrille.ajouterJeton(0, CouleurJeton.JAUNE);
+
+        Assertions.assertFalse(reussite);
     }
 }
