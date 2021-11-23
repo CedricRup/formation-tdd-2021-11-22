7af1bad72ef80ec1a493778f6fb77b0c4246565b
Franck P <testandro.hd@gmail.com>
2021-11-23 14:41:22 +0100

    Test F

---
+++ src/test/java/IhmTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class IhmTest {
+	
+    @Test
+    public void debut_partie_joueur_en_cours_affiche() {
+    	
+    	Ihm ihm = new Ihm();
+    	//ihm.nouvellePartie();
+    	
+    	
+        // Assertions.assertEquals("joueur en cours : Jaune", );
+
+    	
+    	
+    }
+
+}
