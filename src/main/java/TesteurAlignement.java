9346ecce794d822976278a7ae12851d55f1d987a
Tiaw <delemotte.laurent@gmail.com>
2021-11-23 10:26:01 +0100

    fin colonne0

--- src/main/java/TesteurAlignement.java
+++ src/main/java/TesteurAlignement.java
@@ -10,30 +10,29 @@ public class TesteurAlignement {
         if (testerColonne(grille, StatutCase.JAUNE)) {
             return CouleurAlignement.JAUNE;
         }
-        
+
         return CouleurAlignement.AUCUN;
     }
 
     private static boolean testerColonne(Grille grille, StatutCase statutCase) {
-    	return extracted(grille, statutCase) || extracted2(grille, statutCase);
+        for (int i = 0; i < 4; i++) {
+            if (tester4CasesColonne(grille, statutCase, i)) {
+                return true;
+            }
+        }
+        if (tester4CasesColonne(grille, statutCase, 0)) {
+            return true;
+        }
+        return false;
     }
 
-	private static boolean extracted(Grille grille, StatutCase statutCase) {
-		for (int i =0; i < 4; i++ ) {
-    		if ( grille.statutCase(0, i) != statutCase) {
-    			return false;
-    		}
-    	}
-		return true;
-	}
-	
-	private static boolean extracted2(Grille grille, StatutCase statutCase) {
-		for (int i =1; i < 5; i++ ) {
-    		if ( grille.statutCase(0, i) != statutCase) {
-    			return false;
-    		}
-    	}
-		return true;
-	}
+    private static boolean tester4CasesColonne(Grille grille, StatutCase statutCase, int origine) {
+        for (int i = origine; i < 4 + origine; i++) {
+            if (grille.statutCase(0, i) != statutCase) {
+                return false;
+            }
+        }
+        return true;
+    }
 
 }
--- src/test/java/TesteurAlignementTest.java
+++ src/test/java/TesteurAlignementTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TesteurAlignementTest {
-	
+
     @Test
     void il_faut_au_moins_4_jeton_dans_une_colonne() throws Exception {
         Grille grille = new Grille();
@@ -24,20 +24,20 @@ public class TesteurAlignementTest {
 
         Assertions.assertEquals(CouleurAlignement.ROUGE, alignement);
     }
-    
+
     @Test
     void il_faut_4_jeton_rouge_de_suite_dans_une_colonnes() throws Exception {
-    	
+
         Grille grille = new Grille();
-        
+
         grille.ajouterJeton(0, CouleurJeton.ROUGE);
 
         CouleurAlignement alignement = TesteurAlignement.testerGrille(grille);
 
         Assertions.assertEquals(CouleurAlignement.AUCUN, alignement);
-        
+
     }
-    
+
     @Test
     void il_faut_4_jeton_jaune_de_suite_dans_une_colonne() throws Exception {
         Grille grille = new Grille();
@@ -50,7 +50,7 @@ public class TesteurAlignementTest {
 
         Assertions.assertEquals(CouleurAlignement.JAUNE, alignement);
     }
-    
+
     @Test
     void il_faut_4_jeton_jaune_de_suite_dans_une_colonnes() throws Exception {
         Grille grille = new Grille();
@@ -63,7 +63,7 @@ public class TesteurAlignementTest {
 
         Assertions.assertEquals(CouleurAlignement.AUCUN, alignement);
     }
-    
+
     @Test
     void il_faut_4_jeton_jaune_de_suite_dans_unes_colonnes() throws Exception {
         Grille grille = new Grille();
@@ -89,7 +89,7 @@ public class TesteurAlignementTest {
 
         Assertions.assertEquals(CouleurAlignement.AUCUN, alignement);
     }
-    
+
     @Test
     void il_faut_1_jeton_rouge_de_suite_dans_unes_colonnes() throws Exception {
         Grille grille = new Grille();
@@ -103,5 +103,34 @@ public class TesteurAlignementTest {
 
         Assertions.assertEquals(CouleurAlignement.JAUNE, alignement);
     }
-   
+
+    @Test
+    void il_faut_2_jeton_rouge_de_suite_dans_unes_colonnes() throws Exception {
+        Grille grille = new Grille();
+        grille.ajouterJeton(0, CouleurJeton.ROUGE);
+        grille.ajouterJeton(0, CouleurJeton.ROUGE);
+        grille.ajouterJeton(0, CouleurJeton.JAUNE);
+        grille.ajouterJeton(0, CouleurJeton.JAUNE);
+        grille.ajouterJeton(0, CouleurJeton.JAUNE);
+        grille.ajouterJeton(0, CouleurJeton.JAUNE);
+
+        CouleurAlignement alignement = TesteurAlignement.testerGrille(grille);
+
+        Assertions.assertEquals(CouleurAlignement.JAUNE, alignement);
+    }
+
+    /* colonne 1 */
+    @Test
+    void il_faut_4_jeton_rouge_de_suite_dans_la_colonne1() throws Exception {
+        Grille grille = new Grille();
+        grille.ajouterJeton(1, CouleurJeton.ROUGE);
+        grille.ajouterJeton(1, CouleurJeton.ROUGE);
+        grille.ajouterJeton(1, CouleurJeton.ROUGE);
+        grille.ajouterJeton(1, CouleurJeton.ROUGE);
+
+        CouleurAlignement alignement = TesteurAlignement.testerGrille(grille);
+
+        Assertions.assertEquals(CouleurAlignement.ROUGE, alignement);
+    }
+
 }
